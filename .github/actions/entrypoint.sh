468bf373a04b03384d5b8c4eb93396009b9065dd
hamida <hamida1.trimeche@gmail.com>
2022-07-07 00:37:43 +0100

    test3

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -5,10 +5,10 @@ echo $VAULT_PASS > /vault_password_file.txt
 ansible-vault decrypt \
 --vault-password-file=/vault_password_file.txt \
 id_rsa-encrypted \
---output=~/.ssh/id_rsa
+--output= ~/.ssh/id_rsa
 
 chmod 0600 ~/.ssh/id_rsa
 
 ansible-playbook --vault-password-file=/vault_password_file.txt \
 -i 127.0.0.1  \
-ansible-playbook
+../ansible-playbook
