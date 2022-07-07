b3119461c4207939bd287bba31561279709c50aa
hamida <hamida1.trimeche@gmail.com>
2022-07-07 02:23:41 +0100

    feat:ansible

--- .github/actions/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-!/bin/sh
-mkdir ~/.ssh
-echo $VAULT_PASS > /vault_password_file.txt  ansible-vault decrypt \
---vault-password-file=/vault_password_file.txt \
-id_rsa-encrypted \
---output=~/.ssh/id_rsachmod 0600 ~/.ssh/id_rsaansible-playbook --vault-password-file=/vault_password_file.txt \
--i ansible/hosts \
-ansible/deploy.yml
