aef188c05a8b59e7e626591e1bdd653e18f5e6c3
hamida <hamida1.trimeche@gmail.com>
2022-07-07 01:58:42 +0100

    fix:fix

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
-
-echo $VAULT_PASS > /vault_password_file.txt
-ansible-vault decrypt \
+!/bin/sh
+mkdir ~/.ssh
+echo $VAULT_PASS > /vault_password_file.txt  ansible-vault decrypt \
 --vault-password-file=/vault_password_file.txt \
 id_rsa-encrypted \
---output=~/.ssh/id_rsa
-chmod 0600 ~/.ssh/id_rsa
+--output=~/.ssh/id_rsachmod 0600 ~/.ssh/id_rsaansible-playbook --vault-password-file=/vault_password_file.txt \
+-i ansible/hosts \
+ansible/deploy.yml
